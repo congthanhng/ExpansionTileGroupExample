a779ef140f9b372cb0317f76bdbb399651f786c0
Cong Nguyen <cong.nguyen@prismtechinc.io>
2024-07-24 08:05:39 +0700

    third commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4160358367"
+    serviceWorkerVersion: "1390241951"
   }
 });
